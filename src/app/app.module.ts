326173aa22a7253a4efe53b80026aba26a4d3edc
Lourine <lourine.millicent@gmail.com>
2020-08-31 07:21:30 +0300

    Update appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,10 @@ import { UserComponent } from './user/user.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { RepoService } from './repo.service';
 import { from } from 'rxjs';
+import { HighlightDirective } from './highlight.directive';
+import { TimeAgoPipe } from './time-ago.pipe';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { from } from 'rxjs';
     UserComponent,
     RepositoryComponent,
     NavbarComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    HighlightDirective,
+    TimeAgoPipe,
+    
   ],
   imports: [
     BrowserModule,
@@ -25,7 +32,12 @@ import { from } from 'rxjs';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  schemas: [ 
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  providers: [
+    RepoService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
